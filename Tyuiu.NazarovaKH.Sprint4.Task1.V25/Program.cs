c4227325e4b4cca1628b71876cbec71377ec82d2
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2024-12-19 01:00:07 +0300

    Task1

---
+++ Tyuiu.NazarovaKH.Sprint4.Task1.V25/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NazarovaKH.Sprint4.Task1.V25.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint4.Task1.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                              *");
+            Console.WriteLine("********************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите значение " + i + "элемента массива: ");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                                    *");
+            Console.WriteLine($"Сумма чётных элементов равна {ds.Calculate(numsArray)}                         ");
+            Console.WriteLine("********************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
