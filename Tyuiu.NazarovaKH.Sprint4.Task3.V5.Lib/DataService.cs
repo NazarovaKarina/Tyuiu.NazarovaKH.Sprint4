4232ddd4f89a01fde9aa08f00b072c3027600242
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2025-01-01 17:24:07 +0300

    Task3

--- Tyuiu.NazarovaKH.Sprint4.Task3.V5.Lib/DataService.cs
+++ Tyuiu.NazarovaKH.Sprint4.Task3.V5.Lib/DataService.cs
@@ -7,12 +7,11 @@ namespace Tyuiu.NazarovaKH.Sprint4.Task3.V5.Lib
         public int Calculate(int[,] array)
         {
             int rows = array.GetLength(0);
-            int cols = array.GetLength(1);
-
+           
             int sumArray = 0;
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < rows; j++)
             {
-                sumArray += array[0, j];
+                sumArray += array[j, 0];
             }
             return sumArray;
         }
