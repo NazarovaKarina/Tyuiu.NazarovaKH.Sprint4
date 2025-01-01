65254a2d1f2db98c1ff6b77f94737230fe29a667
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2025-01-01 20:29:08 +0300

    Task7

---
+++ Tyuiu.NazarovaKH.Sprint4.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.NazarovaKH.Sprint4.Task7.V8.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint4.Task7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 4;
+            int[,] matrix = new int[rows, columns];
+            string str = "264795863157";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 37;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovaKH.Sprint4.Task7.V8/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.NazarovaKH.Sprint4.Task7.V8.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint4.Task7.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                              *");
+            Console.WriteLine("********************************************************************************");
+
+            string value = "264795863157";
+            int rows = 3;
+            int columns = 4;
+
+            Console.WriteLine($"Строка: {value}");
+            Console.WriteLine($"Размер матрицы: {rows}x{columns}");
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                                    *");
+            Console.WriteLine("********************************************************************************");
+
+            int res = ds.Calculate(rows, columns, value); 
+            Console.WriteLine($"Сумма нечетных элементов: {res}");
+            Console.ReadKey();
+        }
+    }
+}
